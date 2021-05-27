61f8aee12d30389efbd22ae370c4d32d98c1e8bc
Shawn Eion Smith <shawn.eion.work@gmail.com>
2021-05-27 14:30:16 -0400

    Adding disk caching

--- main.go
+++ main.go
@@ -6,29 +6,59 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type (
+	EnvVariables struct {
+		Path string `envconfig:"FILE_PATH"`
+	}
+
+	Request struct {
+		Text string `json:"plain_text"`
+	}
+
+	PostResponse struct {
+		ID string `json:"id"`
+	}
+
+	GetResponse struct {
+		Data string `json:"data"`
+	}
 )
 
 var (
 	pwpairs map[string]string
+	environ EnvVariables
 )
 
-type Request struct {
-	Text string `json:"plain_text"`
-}
-
-type PostResponse struct {
-	ID string `json:"id"`
-}
-
-type GetResponse struct {
-	Data string `json:"data"`
-}
+const (
+	defaultPath   string = "./"
+	pairsFileName string = "data.json"
+)
 
 func main() {
 
 	pwpairs = make(map[string]string)
+
+	err := envconfig.Process("SECRETS", &environ)
+
+	if err != nil {
+		log.Fatalf("Failed to load environment: %v", err)
+	}
+
+	if !strings.HasSuffix(environ.Path, "/") {
+		environ.Path += "/"
+	}
+
+	loadFromFile()
 
 	mux := http.NewServeMux()
 
@@ -37,6 +67,7 @@ func main() {
 	mux.HandleFunc("/", secretHandler)
 	//mux.HandleFunc("/secretHandler/:id", secretGetter)
 
+	go listenToExit()
 	http.ListenAndServe(":8080", mux)
 }
 
@@ -123,4 +154,70 @@ func secretGetter(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write(bytes)
+}
+
+func listenToExit() {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	fmt.Println("awaiting signal")
+	<-done
+	writeToFile()
+	log.Println("Back from export to File, trying to exit")
+	os.Exit(0)
+}
+
+func loadFromFile() {
+	log.Print("In loadFromFile")
+
+	file := environ.Path + pairsFileName
+	if _, err := os.Stat(file); err == nil {
+		bytes, err := os.ReadFile(file)
+		if err != nil {
+			//Failed to read, have to start with an empty map
+			//Not going to blow it up, because it would fail the same way the next time
+			log.Printf("Failed to load the save file, skipping: %v", err)
+		} else if os.IsNotExist(err) {
+			log.Println("File doesn't exist, could be the first run")
+			return
+		}
+
+		err = json.Unmarshal(bytes, &pwpairs)
+
+		if err != nil {
+			log.Println("Failed to unmarshal, truncating the file")
+			//Data corruption? Truncate the file to clear the cache
+			os.Truncate(file, 0)
+		}
+	}
+
+	//We're either in memory or on disk, but not both
+	_ = os.Truncate(file, 0)
+}
+
+func writeToFile() {
+	log.Print("In exportToFile")
+
+	b, err := json.Marshal(&pwpairs)
+
+	if err != nil {
+		log.Printf("Failed to marshal map into json, bailing: %v", err)
+		return
+	}
+
+	file := environ.Path + pairsFileName
+	err = os.WriteFile(file, b, 0644)
+
+	if err != nil {
+		log.Printf("Failed to write file %v", err)
+	}
 }
